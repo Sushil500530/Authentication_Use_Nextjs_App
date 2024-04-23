d5f5f27d4d15dc7fa7e7b7d68d4d26a111d7811a
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-04-23 14:04:44 +0600

    add folder structure and db's for model or other

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-center my-5">Login Page</h1>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-center my-5">Signup page</h1>
+        </div>
+    );
+};
+
+export default page;
