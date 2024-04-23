d8537f5480df8dc9598641631cd6fbdfe06163a8
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-04-23 19:15:47 +0600

    add aboutme route for user find

--- src/app/api/users/aboutme/route.ts
+++ src/app/api/users/aboutme/route.ts
@@ -1,14 +1,19 @@
 import { connect } from "@/dbConfig/dbConfig";
 import { NextRequest, NextResponse } from "next/server"
 import User from '@/models/userModel';
-import bcryptjs from 'bcryptjs';
-import jwt from "jsonwebtoken";
+import { getDataFromToken } from "@/utils/getDataFromToken";
 
-
+connect();
 
 export async function POST(request: NextRequest) {
     try {
         // extract data from token 
+      const userId =  await getDataFromToken(request);
+        const user = User.findOne({_id:userId}).select("-Password")
+
+        // check if there is no user 
+        return NextResponse.json({message: "User found", data:user})
+
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, { status: 500 })
     }
