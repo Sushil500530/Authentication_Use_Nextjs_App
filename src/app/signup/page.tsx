47dd632ba7a5f88219bcb02a82c3613975269687
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-04-24 10:09:14 +0600

    add axios route and react hot toast and add signup function or other

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,23 +1,51 @@
 "use client"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
-export default function SignupPage (){
-    const [user,setUser] = useState({
-        username:"",
-        email:"",
-        password:""
+export default function SignupPage() {
+    const router = useRouter();
+    const [user, setUser] = useState({
+        username: "",
+        email: "",
+        password: ""
     })
 
-    const [buttonDisable,setButtonDisable] = useState(false);
-    const [loading,setLoading] = useState(false)
+    const [buttonDisable, setButtonDisable] = useState(false);
+    const [loading, setLoading] = useState(false);
 
 
+    const handleSignup = async () => {
+        try {
+            setLoading(true)
+            const response = await axios.post("/api/users/signup", user)
+            console.log("Sign -up Success", response.data);
+            toast.success('signup successfully')
+            router.push('/login')
+
+
+        } catch (error: any) {
+            console.log("Signup failed");
+            toast.error(error.message)
+        }
+    }
+
+
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+            setButtonDisable(false)
+        } else {
+            setButtonDisable(true)
+        }
+    }, [user])
 
 
 
     return (
         <div>
             <h1 className="text-2xl font-bold text-center my-5">Signup page</h1>
+            <button onClick={handleSignup}>Click here</button>
         </div>
     );
 };
